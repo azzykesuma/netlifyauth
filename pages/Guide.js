c8efa49006d928fae6ed23a9434ea50ab67df7e4
azzy kesuma <official.azzy@gmail.com>
2022-05-05 08:06:32 +0700

    initial commit

---
+++ pages/Guide.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Guides.module.css';
+
+const Guide = () => {
+    return (
+    <div className={styles.guides}>
+        <h2>All Guides</h2>
+    </div> 
+    );
+}
+ 
+export default Guide;
