8f9fadf2ad8e606a94df12de7cc3acdcaa410151
azzy kesuma <official.azzy@gmail.com>
2022-05-05 09:21:18 +0700

    adding login button

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
 import Navbar from '../Component/Navbar'
+import { AuthContextProvider } from '../stores/Auth'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <AuthContextProvider>
       <Navbar />
       <Component {...pageProps} />
-    </>
+    </AuthContextProvider>
     )
 }
 
---
+++ stores/Auth.js
@@ -0,0 +1,42 @@
+import { createContext, useState, useEffect } from 'react';
+import netlifyIdentity from 'netlify-identity-widget';
+
+const AuthContext = createContext({
+    user : null,
+    login : () => {},
+    logout : () => {},
+    authReady : false
+})
+
+export const AuthContextProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        netlifyIdentity.on('login', user => {
+            setUser(user)
+            netlifyIdentity.close()
+            console.log(`${user} just logged in`);
+        })
+        
+        netlifyIdentity.init()
+    },[])
+
+    const login = () => {
+        netlifyIdentity.open();
+    }
+
+    const context = {
+        user,
+        login
+    }
+
+    return (
+        <AuthContext.Provider
+        value={context}
+        >
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext;
